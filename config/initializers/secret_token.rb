c67f927971afff14011d15996855fd049f36dbf8
Delaine <delaine.lawrence35@gmail.com>
2014-04-23 21:48:07 -0400

    Fixed errors in postgress.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatty::Application.config.secret_key_base = 'd913bc9a38da02fc7bedc367d0f1cfdc4a8db250d0f83c0c52979c370a0f43ecbb9976b83cbb607d80013bd5bf7a7f976165da50fd5651414aaa310adf3cc400'
